63bae629184ba6b41f8c49cc04d547d3526a5358
Luccio2005 <luisernestocruzgonzales1@gmail.com>
2025-08-16 09:42:08 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static int busquedaBinaria(int[]array, int x){
+        int inicio=0;
+        int fin = array.length -1;
+        while (inicio<= fin){
+            int medio = (inicio+fin)/2;
+            if (array[medio]==x){
+                return medio;
+            }
+            if (array[medio] <x){
+                inicio = medio+1;
+            } else {
+                fin = medio-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] numeros = {2,4,6,8,10,12,14};
+        int buscar = 10;
+        int resultado = busquedaBinaria(numeros, buscar);
+        if (resultado != -1){
+            System.out.println("Elemento encontrado en indice: "+resultado);
+        }else {
+            System.out.println("Elemento no encontrado.");
+        }
+        }
+    }
