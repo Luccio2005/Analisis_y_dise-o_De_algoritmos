3205d5b5a3d8e65be4f8f62bbf7d47698ffed92f
Luccio2005 <luisernestocruzgonzales1@gmail.com>
2025-08-17 23:01:56 -0500

    primeros cambios

--- Main.java
+++ Main.java
@@ -1,28 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
-    public static int busquedaBinaria(int[]array, int x){
-        int inicio=0;
-        int fin = array.length -1;
-        while (inicio<= fin){
-            int medio = (inicio+fin)/2;
-            if (array[medio]==x){
+    public static int busquedaBinaria(int[] array, int x) {
+        int inicio = 0;
+        int fin = array.length - 1;
+        while (inicio <= fin) {
+            int medio = inicio + (fin - inicio) / 2;
+            if (array[medio] == x) {
                 return medio;
             }
-            if (array[medio] <x){
-                inicio = medio+1;
+            if (array[medio] < x) {
+                inicio = medio + 1;
             } else {
-                fin = medio-1;
+                fin = medio - 1;
             }
         }
         return -1;
     }
+
     public static void main(String[] args) {
-        int[] numeros = {2,4,6,8,10,12,14};
-        int buscar = 10;
+        int[] numeros = {2, 4, 6, 8, 10, 12, 14};
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese el número a buscar: ");
+        int buscar = sc.nextInt();
+
         int resultado = busquedaBinaria(numeros, buscar);
-        if (resultado != -1){
-            System.out.println("Elemento encontrado en indice: "+resultado);
-        }else {
+        if (resultado != -1) {
+            System.out.println("Elemento encontrado en índice: " + resultado);
+        } else {
             System.out.println("Elemento no encontrado.");
         }
-        }
     }
+}
