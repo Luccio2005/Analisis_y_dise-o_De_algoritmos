459f85bb5ca3878f41ebd819c370199b5e1a71c3
Luccio2005 <luisernestocruzgonzales1@gmail.com>
2025-08-18 23:36:09 -0500

    mejora de busqueda binaria

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static int busquedaBinaria(int[] array, int x) {
         int inicio = 0;
         int fin = array.length - 1;
@@ -22,14 +23,27 @@ public class Main {
         int[] numeros = {2, 4, 6, 8, 10, 12, 14};
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("Ingrese el número a buscar: ");
-        int buscar = sc.nextInt();
 
-        int resultado = busquedaBinaria(numeros, buscar);
-        if (resultado != -1) {
-            System.out.println("Elemento encontrado en índice: " + resultado);
-        } else {
-            System.out.println("Elemento no encontrado.");
+        System.out.println("Array disponible:");
+        for (int num : numeros) {
+            System.out.print(num + " ");
+        }
+        System.out.println("\n");
+
+        System.out.print("¿Cuántos números desea buscar? ");
+        int cantidad = sc.nextInt();
+
+        for (int i = 0; i < cantidad; i++) {
+            System.out.print("Ingrese el número a buscar: ");
+            int buscar = sc.nextInt();
+
+            int resultado = busquedaBinaria(numeros, buscar);
+            if (resultado != -1) {
+                System.out.println("✔ Elemento " + buscar + " encontrado en el índice: " + resultado);
+            } else {
+                System.out.println("✘ Elemento " + buscar + " no encontrado.");
+            }
         }
     }
 }
+
