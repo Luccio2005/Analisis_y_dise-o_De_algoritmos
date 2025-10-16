0b1dadba87464d7aea8d08d7ab490099a9af6fd9
Luccio2005 <luisernestocruzgonzales1@gmail.com>
2025-10-16 12:33:44 -0500

    nose

--- gestionbiblioteca/gestionbiblioteca/src/Libro.java
+++ gestionbiblioteca/gestionbiblioteca/src/Libro.java
@@ -3,34 +3,27 @@ public class Libro {
     private int id;
     private String titulo;
     private String autor;
-
     // Constructor
     public Libro(int id, String titulo, String autor) {
         this.id = id;
         this.titulo = titulo;
         this.autor = autor;
     }
-
     // Getters y Setters
     public int getId() {
         return id;
     }
-
     public String getTitulo() {
         return titulo;
     }
-
     public String getAutor() {
         return autor;
     }
-
     // Método toString para imprimir fácil
     @Override
     public String toString() {
-        return "Libro{" +
-                "id=" + id +
-                ", titulo='" + titulo + '\'' +
-                ", autor='" + autor + '\'' +
-                '}';
+        return "Libro{" + "id=" + id + ", titulo='" + titulo + '\'' + ", autor='" + autor + '\'' + '}';
     }
 }
+
+
